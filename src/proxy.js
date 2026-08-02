2a2db6ab843cdd9e24bc2f2cd637d92a3fa70d76
BrutusSMG <fsousam@hotmail.com>
2026-08-02 15:22:45 -0300

    Ajuste de imagem no subdominio

--- src/proxy.js
+++ src/proxy.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
   ],
 };
 
